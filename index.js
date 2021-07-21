16e01bdb62a79a658c9d3523bc2a8e03612d091e
Destinee Robinson <Destineerobinson08@gmail.com>
2021-07-21 01:13:36 -0400

    commit tree a987d9f1b7c8e0f119f4e08af076567f22818160

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+};
+saturdayFun('bathe my dog');
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+};
+saturdayFun('work from home');
+
+function wrapAdjective(compliment = "*") {
+    function innerFunction (adj = 'special'){
+       return `You are ${compliment}${adj}${compliment}!`
+    }
+        return innerFunction;
+    }
+
+//wrapAdjective(innerFunction + "a dedicated programmer");
+
+
+// function wrapAdjective(flair="*"){
+//     (adj="Special") => `You are ${flair + adj + flair}!`
+// }
